2bfed2158822391dbb6f1eb94ecba5b0fd81058e
Stiven Torres <torresmendozastiven@gmail.com>
2025-02-11 14:23:39 -0500

    se restructuro el archivo api

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -6,8 +6,10 @@ use App\Helpers\ResponseHelper;
 use App\Models\Inventario;
 use App\Models\Materiale;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Throwable;
 
@@ -29,27 +31,32 @@ class InventarioController extends Controller
             return ResponseHelper::error(422, $validator->errors()->first(), $validator->errors());
         }
 
-        if (!is_string($request->referencia_material)) {
+        try {
+            // if (!is_string($request->referencia_material)) {
 
 
-            return ResponseHelper::error(422, "El campo referencia_material no es válido");
+            //     return ResponseHelper::error(422, "El campo referencia_material no es válido");
+            // }
+            $consecutivo = Inventario::where("referencia_material", "=", $request->referencia_material)
+                ->max("consecutivo") ?? 0;
+
+            $inventario =  new Inventario();
+            $inventario->referencia_material = strtoupper(trim($request->referencia_material));
+            // return response()->json($request->referencia_material);
+            $inventario->consecutivo = $consecutivo + 1;
+            $inventario->costo = (float) trim($request->costo);
+            $inventario->cantidad = trim($request->cantidad);
+            $inventario->nit_proveedor = trim($request->nit_proveedor);
+            $inventario->nombre_proveedor = strtoupper(trim($request->nombre_proveedor));
+            $inventario->descripcion_proveedor = "Fer";
+            $inventario->numero_identificacion = Auth::user()->numero_identificacion;
+            $inventario->save();
+
+            return ResponseHelper::success(200, "Se ha registrado con exito", ["inventario" => $inventario]);
+        } catch (Throwable $th) {
+            Log::error("Error al registrar un lote " . $th->getMessage());
+            return ResponseHelper::error(500, "Error interno en el servidor");
         }
-        $consecutivo = Inventario::where("referencia_material", "=", $request->referencia_material)
-            ->max("consecutivo") ?? 0;
-
-        $inventario =  new Inventario();
-        $inventario->referencia_material = strtoupper(trim($request->referencia_material));
-        // return response()->json($request->referencia_material);
-        $inventario->consecutivo = $consecutivo + 1;
-        $inventario->costo = (float) trim($request->costo);
-        $inventario->cantidad = trim($request->cantidad);
-        $inventario->nit_proveedor = trim($request->nit_proveedor);
-        $inventario->nombre_proveedor = strtoupper(trim($request->nombre_proveedor));
-        $inventario->descripcion_proveedor = "Fer";
-        $inventario->numero_identificacion = Auth::user()->numero_identificacion;
-        $inventario->save();
-
-        return ResponseHelper::success(200, "Se ha registrado con exito", ["inventario" => $inventario]);
     }
 
     public function update(Request $request)
@@ -73,7 +80,7 @@ class InventarioController extends Controller
                 ->where("consecutivo", $request->consecutivo)
                 ->first();
 
-           
+
             if (!$inventario) {
 
                 return ResponseHelper::error(404, "Inventario no encontrado");
@@ -82,13 +89,13 @@ class InventarioController extends Controller
 
 
             Inventario::where('referencia_material', $request->referencia_material)
-            ->where('consecutivo', $request->consecutivo)
-            ->update([
-                'costo' => $request->costo_material,
-                'cantidad' => $request->cantidad
-            ]);
-        
-        
+                ->where('consecutivo', $request->consecutivo)
+                ->update([
+                    'costo' => $request->costo_material,
+                    'cantidad' => $request->cantidad
+                ]);
+
+
             return ResponseHelper::success(
                 200,
                 "El inventario se ha actualizado con exito",
@@ -99,5 +106,33 @@ class InventarioController extends Controller
         }
     }
 
-    public function show($referencia_material) {}
+    public function show($referencia_material, $consecutivo)
+    {
+        $validator = Validator::make(["referencia_material" => $referencia_material, "consecutivo" => $consecutivo], [
+            "referencia_material" => "required|string",
+            "consecutivo" => "required|numeric"
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseHelper::error(422, $validator->errors()->first(), $validator->errors());
+        }
+
+        try {
+            $inventarios = Inventario::where("referencia_material", $referencia_material)
+                ->where("consecutivo", $consecutivo)
+                ->get();
+
+            if (!$inventarios) {
+                return ResponseHelper::error(404, "No se ha encontrado inventario de ese material");
+            }
+
+            return ResponseHelper::error(200, "Se ha encontrado inventario de este material", ["inventarios" => $inventarios]);
+        } catch (Throwable $th) {
+            Log::error("Error al consultar un inventario por referencia del material y consecutivo " . $th->getMessage());
+            return ResponseHelper::error(500, "Error interno en el servidor");
+        } catch (ModelNotFoundException $e){
+            
+            return ResponseHelper::error(422, "Error interno en el servidor");
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\AsignacioneController;
 use App\Http\Controllers\InmuebleController;
 use App\Http\Controllers\InventarioController;
@@ -13,81 +14,83 @@ use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('auth/login', action: LoginController::class);
 
-Route::group(['middleware' => ['auth:api'], "prefix" => 'auth'], function () {
-    Route::post('/logout', action: LogoutController::class);
-});
 
-Route::middleware('auth:api')->prefix("usuario")->group(function () {
-    Route::post("/", [UserController::class, 'store']);
-    Route::get("/", [UserController::class, 'index']);
-    Route::get("/{numero_identificacion}", [UserController::class, 'show']);
-    Route::put("/changePassword", [UserController::class, 'changePassword']);
-    Route::put("/changePasswordAdmin", [UserController::class, 'changePasswordAdmin']);
-    Route::put("/{numero_identificacion}", [UserController::class, 'update']);
-    Route::delete("/", [UserController::class, 'destroy']);
-});
-
-Route::middleware('auth:api')->prefix('materiale')->group(function () {
-    Route::get("/", [MaterialeController::class, "index"]);
-    Route::get("/{referencia_material}", [MaterialeController::class, "show"]);
-    Route::post("/", [MaterialeController::class, "store"]);
-    Route::put("/{referencia_material}", [MaterialeController::class, "update"]);
-    Route::delete("/", [MaterialeController::class, 'destroy']);
-    Route::post("/lote", [MaterialeController::class, "storeInventario"]);
-});
-
-Route::middleware('auth:api')->prefix("proyecto")->group(function () {
-    Route::get("/", [ProyectoController::class, 'index']);
-    Route::get("/select", [ProyectoController::class, 'select']);
-    Route::get("/{codigo_proyecto}", [ProyectoController::class, 'show']);
-    Route::get("/presupuesto/{codigo_proyecto}", [ProyectoController::class, 'showWithPresupuesto']);
-    Route::post("/", [ProyectoController::class, 'store']);
-    Route::put("/{codigo_proyecto}", [ProyectoController::class, 'update']);
-    Route::delete("/", [ProyectoController::class, 'destroy']);
+Route::middleware("auth:api")->group(function(){
+    Route::prefix("auth")->group( function () {
+        Route::post('/logout', action: LogoutController::class);
+    });
+    Route::prefix("usuario")->group(function () {
+        Route::post("/", [UserController::class, 'store']);
+        Route::get("/", [UserController::class, 'index']);
+        Route::get("/{numero_identificacion}", [UserController::class, 'show']);
+        Route::put("/changePassword", [UserController::class, 'changePassword']);
+        Route::put("/changePasswordAdmin", [UserController::class, 'changePasswordAdmin']);
+        Route::put("/{numero_identificacion}", [UserController::class, 'update']);
+        Route::delete("/", [UserController::class, 'destroy']);
+    });
+    Route::middleware('auth:api')->prefix('materiale')->group(function () {
+        Route::get("/", [MaterialeController::class, "index"]);
+        Route::get("/{referencia_material}", [MaterialeController::class, "show"]);
+        Route::post("/", [MaterialeController::class, "store"]);
+        Route::put("/{referencia_material}", [MaterialeController::class, "update"]);
+        Route::delete("/", [MaterialeController::class, 'destroy']);
+        Route::post("/lote", [MaterialeController::class, "storeInventario"]);
+    });
     
+    Route::prefix("proyecto")->group(function () {
+        Route::get("/", [ProyectoController::class, 'index']);
+        Route::get("/select", [ProyectoController::class, 'select']);
+        Route::get("/{codigo_proyecto}", [ProyectoController::class, 'show']);
+        Route::get("/presupuesto/{codigo_proyecto}", [ProyectoController::class, 'showWithPresupuesto']);
+        Route::post("/", [ProyectoController::class, 'store']);
+        Route::put("/{codigo_proyecto}", [ProyectoController::class, 'update']);
+        Route::delete("/", [ProyectoController::class, 'destroy']);
+        
+    });
+    
+    Route::prefix("tipo_inmueble")->group(function(){
+        Route::get("/", [TipoInmuebleController::class,'index']);
+        Route::get("/{id}", [TipoInmuebleController::class,'show']);
+        Route::post("/", [TipoInmuebleController::class,'store']);
+        Route::put("/{id}", [TipoInmuebleController::class,'update']);
+        Route::delete("/", [TipoInmuebleController::class,'destroy']);
+    });
+    
+    Route::prefix('inmueble')->group(function(){
+        Route::post("/",[InmuebleController::class,"store"]);
+        Route::get("/",[InmuebleController::class,"index"]);
+        Route::get("/{id}",[InmuebleController::class,"show"]);
+        Route::delete("/", [InmuebleController::class,'destroy']);
+        Route::get("/report/{id}", [InmuebleController::class,"generateCSV"]);
+    });
+    
+    Route::prefix('presupuesto')->group(function () {
+        Route::post("/", [PresupuestoController::class, "store"]);
+        Route::post("/file", [PresupuestoController::class, "fileMasivo"]);
+        Route::get("/", [PresupuestoController::class, "show"]);
+        Route::put("/", [PresupuestoController::class, "update"]);
+        Route::get("/report/{codigo_proyecto}", [PresupuestoController::class, "generateCSV"]);
+        Route::delete("/", [PresupuestoController::class, "destroy"]);
+    });
+    
+    Route::prefix('asignacion')->group(function () {
+        Route::post("/", [AsignacioneController::class, "store"]);
+    });
+    Route::prefix('inventario')->group(function () {
+        Route::post("/", [InventarioController::class, "store"]);
+        Route::get("/{referencia_material}/{consecutivo}",[InventarioController::class,"show"]);
+        Route::put("/",[InventarioController::class,"update"]);
+        
+    });
+
+    Route::fallback(function () {
+        return ResponseHelper::error(404, "La ruta no existe");
+    });
 });
 
-Route::middleware('auth:api')->prefix("tipo_inmueble")->group(function(){
-    Route::get("/", [TipoInmuebleController::class,'index']);
-    Route::get("/{id}", [TipoInmuebleController::class,'show']);
-    Route::post("/", [TipoInmuebleController::class,'store']);
-    Route::put("/{id}", [TipoInmuebleController::class,'update']);
-    Route::delete("/", [TipoInmuebleController::class,'destroy']);
-});
 
-Route::middleware('auth:api')->prefix('inmueble')->group(function(){
-    Route::post("/",[InmuebleController::class,"store"]);
-    Route::get("/",[InmuebleController::class,"index"]);
-    Route::get("/{id}",[InmuebleController::class,"show"]);
-    Route::delete("/", [InmuebleController::class,'destroy']);
-    Route::get("/report/{id}", [InmuebleController::class,"generateCSV"]);
-});
 
-Route::middleware('auth:api')->prefix('presupuesto')->group(function () {
-    Route::post("/", [PresupuestoController::class, "store"]);
-    Route::post("/file", [PresupuestoController::class, "fileMasivo"]);
-    Route::get("/", [PresupuestoController::class, "show"]);
-    Route::put("/", [PresupuestoController::class, "update"]);
-    // Route::delete("/{id}",[PresupuestoController::class,"destroy"]);
-    // Route::get("/",[PresupuestoController::class,"index"]);
-    // Route::get("/{id}",[PresupuestoController::class,"show"]);
-    Route::get("/report/{codigo_proyecto}", [PresupuestoController::class, "generateCSV"]);
-    Route::delete("/", [PresupuestoController::class, "destroy"]);
-});
 
-Route::middleware('auth:api')->prefix('asignacion')->group(function () {
-    Route::post("/", [AsignacioneController::class, "store"]);
-    // Route::delete("/",[AsignacioneController::class,"destroy"]);
-});
 
-Route::middleware('auth:api')->prefix('inventario')->group(function () {
-    Route::post("/", [InventarioController::class, "store"]);
-    // Route::delete("/",[AsignacioneController::class,"destroy"]);
-    Route::put("/",[InventarioController::class,"update"]);
-});
